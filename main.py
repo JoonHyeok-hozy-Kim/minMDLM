732522ade4964ead2573584efe8829055502b522
hozy <hozy5333@gmail.com>
2025-11-06 17:06:10 -0500

    251106 covered with galbook4 :

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from datetime import datetime
 # Hyperparameters for training
 BATCH_SIZE = 64
 NUM_EPOCHS = 10
-STEPS_PER_EPOCH = 10
+STEPS_PER_EPOCH = 1000
 LEARNING_RATE = 5e-4
 MAX_SEQ_LEN_FOR_BATCH = 1024 # Start with 1024
 WANDB_LOG = True
