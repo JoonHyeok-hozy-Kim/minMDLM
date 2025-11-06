f47ebd98ddbfb899a7e79f6282f88251d3bce6a0
hozy <hozy5333@gmail.com>
2025-11-06 16:49:04 -0500

    script bug fixed

--- sbatch_scripts/run_main.sh
+++ sbatch_scripts/run_main.sh
@@ -10,32 +10,28 @@
 #SBATCH --output=/dev/null
 #SBATCH --error=/dev/null
 
+cd ../
+mkdir -p "./logs"
+
+DATE_WITH_TIME=$(date +%Y%m%d_%H%M%S)
+OUTPUT_FILE="./logs/${DATE_WITH_TIME}_run_${SLURM_JOB_ID}.log"
+
+exec > "$OUTPUT_FILE" 2>&1
+
 echo "==================================="
 echo "min_mdlm run"
 date
 echo "Job running on node: $(hostname)"
 echo "==================================="
 
-mkdir -p "../logs"
-
-DATE_WITH_TIME=$(date +%Y%m%d_%H%M%S)
-OUTPUT_FILE="../logs/${DATE_WITH_TIME}_run_${SLURM_JOB_ID}.log"
-
 echo "Attempting to activate venv"
-source ~/miniconda3/bin/activate min_mdlm
+source ./venv/bin/activate
 
 echo "[DEBUG] Conda env check:"
 echo "CONDA_DEFAULT_ENV: $CONDA_DEFAULT_ENV"
 echo "[DEBUG] Python check:"
 python --version
 echo "==================================="
-
-PROJECT_DIR="../"
-echo "Attempting to change directory to: $PROJECT_DIR"
-cd $PROJECT_DIR || { echo "ERROR: Could not find $PROJECT_DIR. Exiting."; exit 1; }
-
-echo "[DEBUG] Current directory check:"
-pwd
 
 python -m main >> "$OUTPUT_FILE" 2>&1
 
