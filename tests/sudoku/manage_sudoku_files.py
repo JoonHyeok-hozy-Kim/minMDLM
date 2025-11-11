7e3b874cd398665a488a87015e95cdd162470977
hozy <hozy5333@gmail.com>
2025-11-11 10:35:21 -0500

    rare version implemented

--- tests/sudoku/manage_sudoku_files.py
+++ tests/sudoku/manage_sudoku_files.py
@@ -36,6 +36,30 @@ def generate_sudoku(num_grids, transform_cnt=20):
         print(f"Error saving samples: {e}")
 
 
+def generate_sudoku_rare(num_grids, transform_cnt=5):
+    # Setup for saving samples
+    dataset_dir = os.path.join(PROJECT_ROOT, "tests", "sudoku", "dataset")
+    os.makedirs(dataset_dir, exist_ok=True)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M")
+    file_name = f"sudoku_rare_dataset_size_{num_grids}_{timestamp}.txt"
+    file_to_write = os.path.join(dataset_dir, file_name)
+    
+    # Create Sudoku
+    S = SudokuGrid()
+    S.create_sudoku_grids(num_grids=num_grids, shuffle_range=True, transform_cnt=transform_cnt, shuffle_coord=True)
+                 
+    try:
+        with open(file_to_write, "a", encoding="utf-8") as f:
+            for i, grid_str in enumerate(S.grids_list):
+                f.write(grid_str)
+                f.write("\n")
+                
+                if i % 1000 == 0:
+                    f.flush()
+    except Exception as e:
+        print(f"Error saving samples: {e}")
+
+
 def read_sudoku_file(file_name):
     dataset_dir = os.path.join(PROJECT_ROOT, "tests", "sudoku", "dataset")
     file_to_read = os.path.join(dataset_dir, file_name)
@@ -56,6 +80,5 @@ def read_sudoku_file(file_name):
 
 
 if __name__ == '__main__':
-    generate_sudoku(1_000_000)
-    # sudoku_grid = read_sudoku_file("sudoku_dataset_size_10000_20251111_0029.txt")
-    # print(len(sudoku_grid.grids_list))
+    # generate_sudoku(1_000_000)
+    generate_sudoku_rare(1)
